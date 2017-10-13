f04aa8a17bce98f7494b54542df1e13e54324c87
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-10-12 21:00:30 -0700

    fixed youtube and button class

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,73 +1,144 @@
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD64s9GqvJU0KWJfIgsexTdScFlVtQv81E",
-    authDomain: "drizzy-b323a.firebaseapp.com",
-    databaseURL: "https://drizzy-b323a.firebaseio.com",
-    projectId: "drizzy-b323a",
-    storageBucket: "drizzy-b323a.appspot.com",
-    messagingSenderId: "649782104967"
-  };
-  firebase.initializeApp(config);
+var songs = {
 
-// Adding firebase into a variable
-  var database = firebase.database();
-//Creating variables
-    var fb = database.ref();
-    var messages = fb.child("messages");
-    var btn = $('button');
-    var wrap = $('.wrapper');
-    var input = $('input.message');
-    var usernameInput = $('input.username');
+        cozy: [
+                {
+                    title: "Sweeterman",
+                    id: "L8mu13y1itU",
+                    weather: "cozy"
+                },
+                {
 
-    var user = [];
+                    title: "Sooner Than Later",
+                    id: "Dbs4XK-dk2U",
+                    weather: "cozy"
+                },
+                {
+                    title: "The Motion",
+                    id: "sddeiggSqhw",
+                    weather: "cozy"
+                }
+            ],
 
-//Cleaning up input text
-    (function($) {
-        $.sanitize = function(input) {
-            var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-            replace(/<[\/\!]*?[^<>]*?>/gi, '').
-            replace(/<style[^>]*?>.*?<\/style>/gi, '').
-            replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
-            return output;
-        };
-    })($); 
+            cold: [
+                {
 
-//Listening when enter key is pressed on username
-    usernameInput.on('keyup', function(e) {
-        if (e.keyCode === 13 && usernameInput.val().length > 0) {
-            var getTxt = usernameInput.val();
-            user.push(getTxt);
-            usernameInput.val('');
-            $('.initModal').css('display', 'none');
-            console.log(user);
+                    title: "Trust Issues",
+                    id: "wqr2L6HXm8w",
+                    weather: "cold"
+                },
+
+                {
+                    title: "I Get Lonely Too",
+                    id: "ifOM0Bx3keM",
+                    weather: "cold"
+                },
+                {
+
+                    title: "Girls Love Beyonce",
+                    id: "aDDk5Th30J4",
+                    weather: "cold"
+                }
+            ],
+
+            warm: [
+                {
+
+                    title: "Sneakin",
+                    id: "WNW1xRqbt94",
+                    weather: "warm"
+                },
+                {
+                    title: "4PM In Calabasas",
+                    id: "ltGltaUs_S4",
+                    weather: "warm"
+                }
+            ]
         }
-    });
+
+        var newPlayer;
 
 
-//Listening when enter key is pressed on message
-    input.on('keyup', function(e) {
-        var curUsername = user.join();
-        if (e.keyCode === 13 && input.val().length > 0) {
-            var getTxt = input.val();
-            messages.push({
-                user: curUsername,
-                message: getTxt
-            });
-            input.val('');
+
+        function chooseSong() {
+
+            var tempSongs = songs[weather];
+
+            var number = Math.floor(Math.random() * (tempSongs.length -1));
+
+            
+
+            var chosenSong = tempSongs[number];
+            console.log(chosenSong.title);
+
+
+
+
+                //create while loop for videos until it shows
+
+
+
+                var tag = document.createElement('script');
+
+                tag.src = "https://www.youtube.com/iframe_api";
+                var firstScriptTag = document.getElementsByTagName('script')[0];
+                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                // 3. function to create an <iframe> and YouTube player
+                //    after the API code downloads
+
+                var player = {
+                    playVideo: function(container, videoId) {
+                      
+                      
+                        if (typeof newPlayer === 'undefined' ) {
+                            console.log(player)
+                            
+
+                            window.onYouTubeIframeAPIReady = function() {
+                                player.loadPlayer(container, videoId);
+                            };
+
+                            $.getScript('https://www.youtube.com/iframe_api');
+                        } else  {
+                            console.log("calledaain")
+                            player.playNext(container, videoId);
+
+                        }
+
+
+                    },
+
+                    //create new method to play
+
+                    playNext: function(container,videoId) {
+
+                      newPlayer.loadVideoById(videoId, 5, "large");
+                      newPlayer.playVideo();
+                    },
+
+                    loadPlayer: function(container, videoId) {
+
+
+                        newPlayer =  new YT.Player(container, {
+                            videoId: videoId,
+                            width: 380,
+                            height: 380,
+                            playerVars: {
+                                autoplay: 1,
+                                controls: 0,
+                                modestbranding: 1,
+                                rel: 0,
+                                showInfo: 0
+                            }
+                        });
+                    }
+                }
+
+                player.playVideo("youtube-box", chosenSong.id);
+
+
+
+            
+
+
         }
-    });
-
-    messages.on("child_added", function(snap) {
-        wrap.append('<li><span>' + $.sanitize(snap.val().user) + ':</span> ' + $.sanitize(snap.val().message) + '</li>');
-        $('.wrapper').scrollTop($('.wrapper')[0].scrollHeight);
-    });
-
-    $("#button").click(function(){
-    if($(this).html() == "_"){
-        $(this).html("+");
-    }
-    else{
-        $(this).html("_");
-    }
-    $("#chatwindow").slideToggle();
-});
